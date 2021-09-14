52e4bc014911e00cc27fbbc8322417392e3d6850
OmarAlbarghouthi <omar.qb8077@gmail.com>
2021-09-14 19:45:32 +0300

    lab03 solved

--- src/App.js
+++ src/App.js
@@ -1,11 +1,59 @@
 import React from "react";
+import Header from "./components/Header";
+import Footer from './components/Footer';
 import HornedBeast from "./components/HornedBeast";
 import Main from "./components/Main";
-class App extends React.Component{
-  render(){
-    return(
+import Data from './components/Data.json';
+import BeastModel from './components/Modal';
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      Data: this.props.Data,
+      animal: {},
+      showModel: false,
+
+
+    }
+
+
+  }
+
+  handleClose = () => {
+    this.setState({ showModel: false })
+  }
+
+  beastButton = (title) => {
+    let selcted = Data.find(animal => {
+
+      if (animal.title === title) {
+        return animal;
+      }
+
+
+    })
+    this.setState({
+      showModel: true,
+      animal: selcted,
+
+    })
+  }
+
+
+  render() {
+    return (
       <>
-      <Main/>
+        <Header />
+        <Main beastButton={this.beastButton} Data={Data} />
+        <BeastModel
+          show={this.state.showModel}
+          handleClose={this.handleClose}
+          animal={this.state.animal}
+          beastButton={this.beastButton}
+        />
+        <Footer />
       </>
 
     )
